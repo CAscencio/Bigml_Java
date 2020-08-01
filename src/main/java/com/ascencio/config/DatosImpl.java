25610e607cdb5cc67d68684991fedee596f0e1ce
Ciurlizza Ascencio <ciurlizza.ascencio@vallegrande.edu.pe>
2020-08-01 00:14:20 -0500

    First Commit

---
+++ src/main/java/com/ascencio/config/DatosImpl.java
@@ -0,0 +1,5 @@
+package com.ascencio.config;
+
+public class DatosImpl {
+
+}
---
+++ src/main/java/com/ascencio/controller/PrediccionController.java
@@ -0,0 +1,11 @@
+package com.ascencio.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+@Named(value = "prediccionController")
+@SessionScoped
+public class PrediccionController implements Serializable {
+
+}
